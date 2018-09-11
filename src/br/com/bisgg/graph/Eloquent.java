5234b5400160a3ab329334bab3b0a5947301c4c1
jsilva <ivaniciojr@gmail.com>
2018-09-10 22:02:25 -0300

    Corrigido o tratamento de erros na lista de linhas do grafo.

--- src/br/com/bisgg/graph/Eloquent.java
+++ src/br/com/bisgg/graph/Eloquent.java
@@ -32,72 +32,72 @@ public class Eloquent extends Persistencia {
        return objects;
     }
 
-    public void getGraphData () throws IOException, ClassNotFoundException {
-        try {
+    public List<String> getGraphData () throws IOException, ClassNotFoundException {
 
-            LinkedList<String> graph_data = this.readGraphFile();
-            List<String> rows = graph_data.stream().filter(line -> line.split("\\s+").length == 2).collect(Collectors.toList());
-            int graphSize = rows.size();
+        LinkedList<String> graph_data = this.readGraphFile();
 
-            int nodes = 0, edges = 0;
-            int currentLine = 0;
+        List<String> rows = graph_data.stream().filter(line -> line.split(" ").length == 2 && !line.equals("-1 -1")).collect(Collectors.toList());
+        int graphSize = rows.size();
 
-            // System.out.println(rows.get(0).split(" ")[1]);
+        int nodes = 0, edges = 0;
 
-            for (int i = 0; i < graphSize; i++) {
-                currentLine++;
+        // Inicializa uma variável de controle de erro. Começa com a hipótese de um grafo válido, caso valor false há erro nas especificações das arestas
+        int i = 0;
+        int indexOfError = 0;
+        int lineOfError = 0;
 
+        boolean state = true;
+
+         /*
+            *
+            * Verifica se o grafo contém o número de nós e arestas, respectivamente, na primeira e segunda linha do arquivo.
+            *
+            * Primeiro fragmento: verifica se a posição que é pra está o nó/aresta está vazia;
+            * Segundo fragmento: verifica a quantidade de caracteres (retirando o espaço) que existe na linha, se for maior
+              igual a 2 significa que aquela informação tende a ser de uma adjacência (V, A) origem/destino;
+            *
+            *
+            * */
+        if (graph_data.get(0).isEmpty() || graph_data.get(0).split(" ").length >= 2) {
+            System.out.println("\bImpossível gerar o grafo.\nHá um erro nas especificações básicas de (V, A) na primeira linha do arquivo.");
+        } else {
+
+            // Obtém a quantidade de nós e arestas do grafo
+            nodes = Integer.parseInt(graph_data.get(0));
+            edges = graph_data.size() - 2;
+
+            while (i < graphSize) {
                 /*
-                *
-                * Verifica se o grafo contém o número de nós e arestas, respectivamente, na primeira e segunda linha do arquivo.
-                *
-                * Primeiro fragmento: verifica se a posição que é pra está o nó/aresta está vazia;
-                * Segundo fragmento: verifica a quantidade de caracteres (retirando o espaço) que existe na linha, se for maior
-                  igual a 2 significa que aquela informação tende a ser de uma adjacência (V, A) origem/destino;
-                *
-                *
-                * */
-                if (graph_data.get(0).isEmpty() || graph_data.get(0).split(" ").length >= 2) {
-                    System.out.println("\bImpossível gerar o grafo.\nHá um erro nas especificações básicas de (V, A) nas duas primeiras linhas do arquivo.");
-                    break;
-                } else {
-                    // Obtém a quantidade de nós e arestas do grafo
-                    nodes = Integer.parseInt(graph_data.get(0));
-                    edges = graph_data.size() - 2;
-                }
+                 *
+                 * Verifica se a linha atual é uma linha que contém informações sobre adjacência do tipo 1 2;
+                 * A especificação mínima é estar, pelo menos, na segunda linha e ela ser diferente de -1 -1, onde identifica ser o fim do arquivo
+                 * */
+                if (!(rows.get(i).equals("-1 -1"))) {
 
-                /*
-                *
-                * Verifica se a linha atual é uma linha que contém informações sobre adjacência do tipo 1 2;
-                * A especificação mínima é estar, pelo menos, na segunda linha e ela ser diferente de -1 -1, onde identifica ser o fim do arquivo
-                * */
-                if (currentLine >= 2 && !(rows.get(i).equals("-1 -1"))) {
-
-                    boolean error = false;
-                    int indexOfError = 0;
-                    int lineOfError = 0;
-
-
-                    if (rows.get(i).split(" ").length <= 1) {
+                    /*
+                     *
+                     * Erro nas especificações das arestas
+                     *
+                     * Se o número de arestas (tamanho da lista) for menor que o numero de arestas esperadas dentro das ligações do arquivo,
+                       retorna um erro, pois em algum momento a expressão lambda decidiu, por meio das especificações
+                       da condição, não filtrar aquele item sem um vértice de destino e/ou origem, logo, para a condição ser satisfeita, o número de arestas
+                       esperada no tamanho da lista de GraphData deve ser do tamanho da lista de Rows, onde as mesmas foram filtradas;
+                     *
+                     * */
+                    if (rows.size() < edges) {
                         indexOfError = i - 1;
                         lineOfError = i;
-                        error = true;
-                    } else {
-
-                        /*
-                        *
-                        * todo:
-                        *   - Adicionar cada linha de cada vértice de origem/destino em uma fila;
-                        *   - Desinfileirar cada elemento da fila paralelamente e passar pro objeto de nós;
-                        * */
+                        state = false;
                     }
-
-                   if (error) System.out.println("\bImpossível gerar o grafo.\nHá um erro na adjacência da posição " + indexOfError + " do arquivo.\n=> G(" + graph_data.get(lineOfError) + ", ?)");
                 }
-            }
 
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+                i++;
+            }
         }
+
+        if (!state)
+            System.out.println("\bImpossível gerar o grafo.\nHá um erro na adjacência da posição " + indexOfError + " do arquivo.\n=> G(" + graph_data.get(lineOfError) + ", ?)");
+
+       return rows;
     }
 }
